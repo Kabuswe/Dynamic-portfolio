421c6a3162994c7efcfebf142535c9d9e65875a8
Kabuswe Mulenga <44984269+Kabuswe@users.noreply.github.com>
2020-11-19 17:05:43 +0000

    Added comments /middleware

--- middleware/database.js
+++ middleware/database.js
@@ -2,14 +2,24 @@ import nextConnect from 'next-connect'
 import { MongoClient } from 'mongodb'
 import {config} from './config';
 
+//Instantiate new mongoDB client
 const client = new MongoClient(config.mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 
+/**
+ * getData function used in each getStaticProps for all the 
+ * routes ( '/', '/experience', '/education', '/skills', '/projects', '/certifications')
+ * 
+ * Each route makes an asynchronous call to the database and retrieves data 
+ * (The above action is according to Next.js' Static generation with data)
+ * 
+ */
 export async function getData(path_name){
   let params = {}
 
+  //Check path_name and return an object containing database field names required by the specific path_name
   switch(path_name){
     case 'home':
       params = {
@@ -43,9 +53,14 @@ export async function getData(path_name){
       break
   }
 
+  /**
+   * Check if connection to the MongoDB database has already been established 
+   * If not connected invoke the connect() method on the client object
+   * **/
   if (!client.isConnected())
     await client.connect()
 
+  //Return data specific to the user profile (profile_id) and the list of fields (params)
   const resp = await
   client
   .db('DynamicPortfolio')
@@ -58,6 +73,7 @@ export async function getData(path_name){
 
 }
 
+//Setup MongoDB as middleware to be used with an object of the (next-connect) module
 async function database(req, res, next) {
   if (!client.isConnected()) await client.connect()
   req.dbClient = client
